ae40ba5a678af42453711129fc100353025958ff
nihachuu <159898387+nihachuu@users.noreply.github.com>
2024-02-13 18:06:13 +0800

    kozen.js

--- .js
+++ .js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "Yayyy!! crush mo'ko talaga :3";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
